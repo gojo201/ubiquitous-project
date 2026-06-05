ad7b03026391845d33fc40bce27dbcf40e92369e
Athina <apap2004@gmail.com>
2026-06-05 21:30:04 +0200

    design: colors typography spacing

--- app/src/main/java/com/tcsappdev/ubiquitous/ui/theme/Color.kt
+++ app/src/main/java/com/tcsappdev/ubiquitous/ui/theme/Color.kt
@@ -2,10 +2,20 @@ package com.tcsappdev.ubiquitous.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Green80 = Color(0xFF4CAF50)
+val Green60 = Color(0xFF388E3C)
+val Green40 = Color(0xFF1B5E20)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LightGreen80 = Color(0xFF8BC34A)
+val LightGreen40 = Color(0xFF558B2F)
+
+val Gray90 = Color(0xFFF5F5F5)
+val Gray60 = Color(0xFF757575)
+val Gray20 = Color(0xFF212121)
+
+val Red = Color(0xFFB00020)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
---
+++ app/src/main/java/com/tcsappdev/ubiquitous/ui/theme/Spacing.kt
@@ -0,0 +1,10 @@
+package com.tcsappdev.ubiquitous.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Spacing {
+    val small = 8.dp
+    val medium = 16.dp
+    val large = 24.dp
+    val extraLarge = 32.dp
+}
